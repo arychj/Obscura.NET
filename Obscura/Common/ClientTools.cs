d87c25718cf220be264acac9a910df6c280ec105
Erik J. Olson <arych@arych.com>
2013-04-05 13:36:45 -0400

    Added Common.ClientTools()

---
+++ Obscura/Common/ClientTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Obscura.Common {
+
+    public class ClientTools {
+
+        /// <summary>
+        /// Returns the client's IP Address from the active HttpContext
+        /// </summary>
+        /// <returns>The client's IP address</returns>
+        public static string IPAddress() {
+            return IPAddress(HttpContext.Current.Request);
+        }
+
+        /// <summary>
+        /// Returns the client's IP Address
+        /// </summary>
+        /// <param name="request">The current HttpRequest object</param>
+        /// <returns>The client's IP address</returns>
+        public static string IPAddress(HttpRequest request) {
+            return request.ServerVariables["REMOTE_ADDR"];
+        }
+    }
+}
