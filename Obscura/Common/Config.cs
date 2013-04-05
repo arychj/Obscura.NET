f55d710825644f16f5c9fa5504850c72cd88b8e2
Erik J. Olson <arych@arych.com>
2013-04-05 12:41:09 -0400

    Added Common.Config() class

---
+++ Obscura/Common/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obscura.Common {
+    internal class Config {
+        private const string CONNECTION_STRING_KEY = "ObscuraConnectionString";
+
+        internal static string ConnectionString {
+            get { return ConfigurationManager.ConnectionStrings[CONNECTION_STRING_KEY].ToString(); }
+        }
+
+        /// <summary>
+        /// Gets an setting from the web.config
+        /// </summary>
+        /// <param name="key">the key to get</param>
+        /// <returns>the setting</returns>
+        internal static string GetSetting(string key) {
+            return ConfigurationManager.AppSettings.Get(key);
+        }
+    }
+}
