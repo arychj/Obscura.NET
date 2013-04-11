011d6c8febe2fe5326f26716ce5f32db1563f59e
Erik J. Olson <arych@arych.com>
2013-04-11 15:42:53 -0400

    Added AdminSite project

---
+++ Site/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+using Obscura.Entities;
+
+namespace Site {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Image image = Image.Create(@"C:\Users\eolson\Desktop\images\alpha.jpg");
+            Response.Clear();
+            Response.Write(image.Id);
+        }
+    }
+}
