ef5b311a777189a75d7357cd39d4d66f2776db1e
Erik J. Olson <arych@arych.com>
2013-04-13 17:53:40 -0400

    Built out Album() class

--- Obscura/Entities/Album.cs
+++ Obscura/Entities/Album.cs
@@ -1,38 +1,175 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Obscura.Common;
+
 namespace Obscura.Entities {
+
+    /// <summary>
+    /// A Photo Album
+    /// </summary>
     public class Album : Entity {
-        private List<Photo> _photos;
+        private bool _loaded = false;
+
+        private EntityCollection<Photo> _photos;
         private Image _thumbnail, _cover;
 
         #region accessors
 
+        /// <summary>
+        /// The thumbnail Image associated with this Album
+        /// </summary>
         public Image Thumbnail {
-            get { return _thumbnail; }
+            get { 
+                Load();
+                return _thumbnail; 
+            }
         }
 
+        /// <summary>
+        /// The cover Image associated with this Album
+        /// </summary>
         public Image Cover {
-            get { return _cover; }
+            get {
+                Load();
+                return _cover;
+            }
         }
 
-        public List<Photo> Photos {
-            get { return _photos; }
+        /// <summary>
+        /// The collection of Photos associated with this Album
+        /// </summary>
+        public EntityCollection<Photo> Photos {
+            get {
+                Load();
+                return _photos;
+            }
         }
 
         #endregion
 
-        public Album(int id)
-            : base(id) {
-            //TODO: constructor
+        /// <summary>
+        /// Constructor
+        /// Retrieves an Album
+        /// </summary>
+        /// <param name="id">the id of the Album</param>
+        internal Album(int id) : this(id, false) { }
+
+        /// <summary>
+        /// Constructor
+        /// Retrieves an Album
+        /// </summary>
+        /// <param name="id">the id of the Album</param>
+        /// <param name="loadImmediately">Load the Photo's contents immediately</param>
+        internal Album(int id, bool loadImmediately)
+            : base(id, loadImmediately) {
+
+                if (loadImmediately)
+                    Load();
         }
 
-        public static Album Create() {
-            //TODO: create
-            return null;
+        /// <summary>
+        /// Constructor
+        /// Builds an Album using the specified parameters
+        /// </summary>
+        /// <param name="entity">the base Entity of this Album</param>
+        /// <param name="cover">the cover Image associated with this Album</param>
+        /// <param name="thumbnail">the thumbnail Image associated with this Album</param>
+        /// <param name="photos">the collection of Photos associated with this album</param>
+        internal Album(Entity entity, Image cover, Image thumbnail, EntityCollection<Photo> photos)
+            : base(entity) {
+                _cover = cover;
+                _thumbnail = thumbnail;
+                _photos = photos;
+                _loaded = true;
+        }
+
+        /// <summary>
+        /// Updates this Album
+        /// Specify null for any parameters to not update
+        /// </summary>
+        /// <param name="thumbnailid">the id of the thumbnail Image associated with this Album</param>
+        /// <param name="coverid">the id of the cover Image associated with this Album</param>
+        public void Update(int? thumbnailid, int? coverid) {
+            string resultcode = null;
+
+            using (ObscuraLinqDataContext db = new ObscuraLinqDataContext(Config.ConnectionString)) {
+                db.xspUpdateAlbum(base.Id, coverid, thumbnailid, ref resultcode);
+
+                if (resultcode == "SUCCESS") {
+                    if (thumbnailid != null)
+                        _thumbnail = new Image((int)thumbnailid);
+                    if (coverid != null)
+                        _cover = new Image((int)coverid);
+                }
+                else
+                    throw new ObscuraException(string.Format("Unable to update Album Entity ID {0}. ({1})", base.Id, resultcode));
+            }
+        }
+
+        /// <summary>
+        /// Loads the Album's details from the database
+        /// </summary>
+        private void Load() {
+            if (!_loaded) {
+                int? coverid = null, thumbid = null;
+                string resultcode = null;
+
+                using (ObscuraLinqDataContext db = new ObscuraLinqDataContext(Config.ConnectionString)) {
+                    db.xspGetAlbum(base.Id, 0, null, ref thumbid, ref coverid, ref resultcode);
+
+                    if (resultcode == "SUCCESS") {
+                        _cover = new Image((int)coverid);
+                        _thumbnail = new Image((int)thumbid);
+                        _photos = new EntityCollection<Photo>(base.Id);
+                    }
+                    else
+                        throw new ObscuraException(string.Format("Album Entity Id {0} does not exist. ({1})", base.Id, resultcode));
+
+                }
+
+                _loaded = true;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new Album
+        /// </summary>
+        /// <param name="title">the title of the Album</param>
+        /// <param name="description">the description of the Album</param>
+        /// <param name="cover">the cover Image of the Album</param>
+        /// <param name="thumbnail">the thumbnail Image of the Album</param>
+        /// <returns>a new Album</returns>
+        public static Album Create(string title, string description, Image cover, Image thumbnail) {
+            Album album = null;
+            Entity entity;
+            string resultcode = null;
+
+            using (ObscuraLinqDataContext db = new ObscuraLinqDataContext(Config.ConnectionString)) {
+                entity = Entity.Create(EntityType.Album, title, description);
+                db.xspUpdateAlbum(entity.Id, cover.Id, thumbnail.Id, ref resultcode);
+
+                if (resultcode == "SUCCESS") {
+                    album = new Album(entity, cover, thumbnail, new EntityCollection<Photo>(entity.Id));
+                }
+                else
+                    throw new ObscuraException(string.Format("Unable to create Album. ({0})", resultcode));
+            }
+
+            return album;
+        }
+
+        /// <summary>
+        /// Retrieves the specified Album
+        /// </summary>
+        /// <param name="id">the id of the Album to retrieve</param>
+        /// <returns>the Album</returns>
+        new public static Album Retrieve(int id) {
+            return new Album(id, true);
         }
     }
 }
